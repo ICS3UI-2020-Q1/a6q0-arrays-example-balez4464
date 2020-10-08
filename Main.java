e6fad2985c4fedb488c5338e9a23d41a59fbd347
balez4464 <balez4464@wrdsb.ca>
2020-10-08 12:50:19 +0000

    example for arrays

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This is an example for arrays
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +10,36 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //constant for number of marks
+    final int NUM_MARKS = 5;
+
+    //creates an array with 5 spots
+    double[] grades = new double[NUM_MARKS];
+
+    //put grades into array using loop
+    for (int i = 0; i < grades.length; i++){
+      System.out.println("Please enter in grade " + (i+1));
+      grades[i] = input.nextDouble();
+    }
+    //create a variable to store a sum
+    double sum = 0;
+
+    //use a for loop to go through the array and add sum
+    for(int i = 0; i < grades.length; i++){
+      //add the grade to sum
+      sum = sum + grades[i];
+    }
+
+    //determine the average
+    double average = sum / NUM_MARKS;
+
+    //let the user know
+    System.out.println("Your average grade is " + average);
+
     
+
   }
 }
